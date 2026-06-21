1bdc5fe49578ef627fe747c286d6b5233a63d136
hoyermaik-rgb <hoyer.maik@gmail.com>
2026-06-21 21:30:47 +0000

    Deploying to gh-pages from @ hoyermaik-rgb/Ralf-Arbeitszeit@ee5c12530405c89c65aee53ca2d504861173a688 🚀

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Simple Service Worker for PWA compliance (enabling 'Add to Home Screen')
+const CACHE_NAME = 'zeiterfassung-cache-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request).catch(() => {});
+    })
+  );
+});
